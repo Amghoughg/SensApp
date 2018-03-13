5b9e8fc5fae6d0b5790035e244ebf6fd97daee4d
mattia <mattia@mcampana.iit.cnr.it>
2018-03-13 17:28:37 +0100

    .

--- app/src/main/java/it/cnr/iit/sensapp/CustomApplication.java
+++ app/src/main/java/it/cnr/iit/sensapp/CustomApplication.java
@@ -4,15 +4,25 @@ import android.app.Application;
 
 import com.twitter.sdk.android.core.Twitter;
 
-/**
- * Created by mattia on 27.01.18.
- */
+import org.acra.ACRA;
+import org.acra.annotation.AcraCore;
+import org.acra.annotation.AcraHttpSender;
+import org.acra.data.StringFormat;
+import org.acra.sender.HttpSender;
 
+@AcraCore(buildConfigClass = BuildConfig.class, reportFormat= StringFormat.JSON)
+@AcraHttpSender(
+        httpMethod = HttpSender.Method.PUT,
+        uri = "http://mcampana.iit.cnr.it:5984/acra-mydigitalfootprint/_design/acra-storage/_update/report",
+        basicAuthLogin = "mydigitalfootprint",
+        basicAuthPassword = "mydigitalfootprint"
+)
 public class CustomApplication extends Application {
 
     public void onCreate() {
         super.onCreate();
 
+        ACRA.init(this);
         Twitter.initialize(this);
     }
 
